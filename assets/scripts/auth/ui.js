e1a958d4f3b3099d34ddb9f8722634db832ff790
louisbookoff <lsbookof@gmail.com>
2018-04-18 15:19:19 -0400

    Added Toast Messaging to application
    - added toast npm
    - Created messaging for all user actions
    - Removed old user messaging

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -3,37 +3,64 @@
 const store = require('../store')
 const showHandlebars = require('../templates/list.hbs')
 const events = require('./events')
+require('./../../../node_modules/jquery-toast-plugin/src/jquery.toast.js')
+require('./../../../node_modules/jquery-toast-plugin/src/jquery.toast.css')
 
-const goTop = function () {
-  $('html, body').animate({ scrollTop: 0 }, 'fast')
-}
+// const goTop = function () {
+//   $('html, body').animate({ scrollTop: 0 }, 'fast')
+// }
 
 const signUpSuccess = function (data) {
   // if ($(.form-group).val() === $(.confirmation).val())
-  $('#message').text('Signed up successful!')
-  $('#message').css('background-color', 'green')
   $('.close').click()
-  $('#exampleInputEmail1').val('')
-  $('#exampleInputPassword1').val('')
-  $('#exampleInputPasswordConfirmation').val('')
-  goTop()
+  $('#email').val('')
+  $('#password-conf').val('')
+  $('#confirm-password').val('')
+  // goTop()
   // console.log(data)
+
+  $.toast({
+    text: 'You have signed up for briefs',
+    heading: 'Sign up success!',
+    icon: 'success',
+    showHideTransition: 'slide',
+    allowToastClose: true,
+    hideAfter: 3000,
+    stack: 5,
+    position: 'bottom-left',
+    textAlign: 'left',
+    loader: true,
+    loaderBg: '#fed',
+    bgColor: '#green',
+    textColor: 'black'
+  })
 }
 
 const signUpFailure = function () {
-  $('#message').text('Error on sign up')
-  $('#message').css('background-color', 'red')
-  $('#exampleInputEmail1').val('')
-  $('#exampleInputPassword1').val('')
-  $('#exampleInputPasswordConfirmation').val('')
+  $('#email').val('')
+  $('#password-conf').val('')
+  $('#confirm-password').val('')
   $('.close').click()
-  goTop()
-  // console.error(error)
+  // goTop()
+
+  $.toast({
+    text: 'Make sure you entered in the correct info!',
+    heading: 'Sign up failure!',
+    icon: 'warning',
+    showHideTransition: 'slide',
+    allowToastClose: true,
+    hideAfter: 3000,
+    stack: 5,
+    position: 'top-left',
+    textAlign: 'left',
+    loader: true,
+    loaderBg: 'red',
+    bgColor: 'red',
+    textColor: 'black'
+  })
 }
 
 const signInSuccess = function (data) {
-  $('#message').text('Signed in succesfully')
-  $('#message').css('background-color', 'green')
   // keep but change id for this and temporarily commenting out below
   $('#dashboard').removeClass('hidden')
   // console.log(data)
@@ -43,67 +70,195 @@ const signInSuccess = function (data) {
   $('.close').click()
   $('#InputEmail1').val('')
   $('#InputPass1').val('')
-  goTop()
+
+  $.toast({
+    text: 'You have signed in!',
+    heading: 'Sign in success!',
+    icon: 'success',
+    showHideTransition: 'slide',
+    allowToastClose: true,
+    hideAfter: 3000,
+    stack: 5,
+    position: 'bottom-left',
+    textAlign: 'left',
+    loader: true,
+    loaderBg: '#fed',
+    bgColor: '#green',
+    textColor: 'black'
+  })
+  //
+  // goTop()
 }
 const signInFailure = function () {
-  $('#message').text('Error on sign in, try again!')
-  $('#message').css('background-color', 'red')
+  // $('#message').text('Error on sign in, try again!')
+  // $('#message').css('background-color', 'red')
   $('#InputEmail1').val('')
   $('#InputPass1').val('')
   $('.close').click()
   // console.error(error)
-  goTop()
+  // goTop()
+
+  $.toast({
+    text: 'Make sure you entered in the correct information!',
+    heading: 'Sign in failure!',
+    icon: 'warning',
+    showHideTransition: 'slide',
+    allowToastClose: true,
+    hideAfter: 3000,
+    stack: 5,
+    position: 'top-left',
+    textAlign: 'left',
+    loader: true,
+    loaderBg: 'red',
+    bgColor: 'red',
+    textColor: 'black'
+  })
 }
 
 const changePasswordSuccess = function () {
-  $('#message').text('You have changed your password succesfully')
-  $('#message').css('background-color', 'green')
+  // $('#message').text('You have changed your password succesfully')
+  // $('#message').css('background-color', 'green')
   $('#password-change-field').val('')
   $('#password-confirm-change').val('')
   // console.log('change password successfully')
-  goTop()
+  // goTop()
+  $.toast({
+    text: 'You have succesfully change your password!',
+    heading: 'Change Password Success!',
+    icon: 'success',
+    showHideTransition: 'slide',
+    allowToastClose: true,
+    hideAfter: 3000,
+    stack: 5,
+    position: 'bottom-left',
+    textAlign: 'left',
+    loader: true,
+    loaderBg: '#fed',
+    bgColor: '#green',
+    textColor: 'black'
+  })
 }
 
-const changePasswordFailure = function (error) {
-  $('#message').text('Error changing password')
-  $('#message').css('background-color', 'red')
-  $('#old-password').val('')
-  $('#new-password').val('')
-  console.error(error)
-  goTop()
+const changePasswordFailure = function () {
+  // $('#message').text('Error changing password')
+  // $('#message').css('background-color', 'red')
+  $('#password-change-field').val('')
+  $('#password-confirm-change').val('')
+  // console.error(error)
+  // goTop()
+
+  $.toast({
+    text: 'Make sure you entered in the correct information!',
+    heading: 'Change Password Failure',
+    icon: 'warning',
+    showHideTransition: 'slide',
+    allowToastClose: true,
+    hideAfter: 3000,
+    stack: 5,
+    position: 'top-left',
+    textAlign: 'left',
+    loader: true,
+    loaderBg: 'red',
+    bgColor: 'red',
+    textColor: 'black'
+  })
 }
 
 const signOutSuccess = function () {
-  $('#message').text('You have signed out succesfully')
-  $('#message').css('background-color', 'green')
+  // $('#message').text('You have signed out succesfully')
+  // $('#message').css('background-color', 'green')
   $('#dashboard').toggleClass('hidden')
   $('#pageone').removeClass('hidden')
   $('.content').empty()
   // console.log('Signed out successfully')
-  goTop()
+  // goTop()
+
+  $.toast({
+    text: 'You Have Succesfully Signed Out. Visit us soon!',
+    heading: 'Change Password Success!',
+    icon: 'success',
+    showHideTransition: 'slide',
+    allowToastClose: true,
+    hideAfter: 3000,
+    stack: 5,
+    position: 'top-left',
+    textAlign: 'left',
+    loader: true,
+    loaderBg: '#fed',
+    bgColor: '#green',
+    textColor: 'black'
+  })
 }
 
-const signOutFailure = function (error) {
-  $('#message').text('Error on sign out')
-  $('#message').css('background-color', 'red')
-  console.error(error)
-  goTop()
+const signOutFailure = function () {
+  // $('#message').text('Error on sign out')
+  // $('#message').css('background-color', 'red')
+  // console.error(error)
+  // goTop()
+
+  $.toast({
+    text: 'Check your connection',
+    heading: 'Sign Out Failure',
+    icon: 'warning',
+    showHideTransition: 'slide',
+    allowToastClose: true,
+    hideAfter: 3000,
+    stack: 5,
+    position: 'top-left',
+    textAlign: 'left',
+    loader: true,
+    loaderBg: 'red',
+    bgColor: 'red',
+    textColor: 'black'
+  })
 }
 
 const createBriefSuccess = function (data) {
-  $('#message').text('You have sucessfully created a trip!')
-  $('#message').css('background-color', 'green')
+  // $('#message').text('You have sucessfully created a trip!')
+  // $('#message').css('background-color', 'green')
   // console.log(events)
   $('.content').empty()
   $('.update').val('')
-  goTop()
+  // goTop()
+
+  $.toast({
+    text: 'You Have Succesfully Created A Brief!',
+    heading: 'Brief Created!',
+    icon: 'success',
+    showHideTransition: 'slide',
+    allowToastClose: true,
+    hideAfter: 3000,
+    stack: 5,
+    position: 'bottom-left',
+    textAlign: 'left',
+    loader: true,
+    loaderBg: '#fed',
+    bgColor: '#green',
+    textColor: 'black'
+  })
 }
 
 const createBriefFailure = function () {
-  $('#message').text('Error while creating a trip')
-  $('#message').css('background-color', 'red')
+  // $('#message').text('Error while creating a trip')
+  // $('#message').css('background-color', 'red')
   $('.update').val('')
-  goTop()
+  // goTop()
+
+  $.toast({
+    text: 'Make sure all fields are correct and your connection is strong',
+    heading: 'Brief Creation Failed',
+    icon: 'warning',
+    showHideTransition: 'slide',
+    allowToastClose: true,
+    hideAfter: 3000,
+    stack: 5,
+    position: 'bottom-left',
+    textAlign: 'left',
+    loader: true,
+    loaderBg: 'red',
+    bgColor: 'red',
+    textColor: 'black'
+  })
 }
 
 const getBriefsSuccess = function (data) {
@@ -112,54 +267,156 @@ const getBriefsSuccess = function (data) {
   $('#content').html(showBriefsHtml)
   const elements = data.briefs
   if (elements.length === 0) {
-    $('#message').text('You have no briefs, the table below is empty can\'t you see?')
-    $('#message').css('background-color', 'red')
+    $.toast({
+      text: 'You have no briefs, please create some.',
+      heading: 'No Briefs Have Been Created',
+      icon: 'warning',
+      showHideTransition: 'slide',
+      allowToastClose: true,
+      hideAfter: 3000,
+      stack: 5,
+      position: 'bottom-left',
+      textAlign: 'left',
+      loader: true,
+      loaderBg: 'red',
+      bgColor: 'red',
+      textColor: 'black'
+    })
   } else {
-    $('#message').text('Below are your briefs. Note: If there are none in the table try creating some.')
-    $('#message').css('color', 'white')
+    $.toast({
+      text: 'Checkout Your Briefs!',
+      heading: 'Below Are Your Briefs!',
+      icon: 'success',
+      showHideTransition: 'slide',
+      allowToastClose: true,
+      hideAfter: 3000,
+      stack: 5,
+      position: 'top-left',
+      textAlign: 'left',
+      loader: true,
+      loaderBg: '#fed',
+      bgColor: '#green',
+      textColor: 'black'
+    })
   }
 }
 
 const getBriefsFailure = function (data) {
-  $('#message').text('You have no briefs')
-  $('#message').css('background-color', 'red')
-  goTop()
+  $.toast({
+    text: 'Check your connection',
+    heading: 'Brief Retrieval Failed',
+    icon: 'warning',
+    showHideTransition: 'slide',
+    allowToastClose: true,
+    hideAfter: 3000,
+    stack: 5,
+    position: 'top-left',
+    textAlign: 'left',
+    loader: true,
+    loaderBg: 'red',
+    bgColor: 'red',
+    textColor: 'black'
+  })
 }
 
 const updateBriefSuccess = function (data) {
-  $('#message').text('You have sucessfully updated that brief!')
-  $('#message').css('background-color', 'green')
+  // $('#message').text('You have sucessfully updated that brief!')
+  // $('#message').css('background-color', 'green')
   $('.content').empty()
   store.brief = data.brief
-  goTop()
+  // goTop()
   $('#testing-this-modal').modal('toggle')
   $('.title-class').val('')
   $('.subject-class').val('')
   $('.body-class').val('')
+
+  $.toast({
+    text: 'You Have Succesfully Updated Your Brief!',
+    heading: 'Brief Updated!',
+    icon: 'success',
+    showHideTransition: 'slide',
+    allowToastClose: true,
+    hideAfter: 3000,
+    stack: 5,
+    position: 'bottom-left',
+    textAlign: 'left',
+    loader: true,
+    loaderBg: '#fed',
+    bgColor: '#green',
+    textColor: 'black'
+  })
 }
 
 const updateBriefFailure = function () {
-  $('#message').text('Error while updating brief')
-  $('#message').css('background-color', 'red')
+  // $('#message').text('Error while updating brief')
+  // $('#message').css('background-color', 'red')
   $('.content').empty()
-  goTop()
+  // goTop()
   $('.title-class').val('')
   $('.subject-class').val('')
   $('.body-class').val('')
+
+  $.toast({
+    text: 'Check your connection and make sure the correct info has been entered',
+    heading: 'Brief Update Failed',
+    icon: 'warning',
+    showHideTransition: 'slide',
+    allowToastClose: true,
+    hideAfter: 3000,
+    stack: 5,
+    position: 'top-left',
+    textAlign: 'left',
+    loader: true,
+    loaderBg: 'red',
+    bgColor: 'red',
+    textColor: 'black'
+  })
 }
 
 const deleteBriefSuccess = function () {
-  $('#message').text('Brief deleted')
-  $('#message').css('background-color', 'green')
+  // $('#message').text('Brief deleted')
+  // $('#message').css('background-color', 'green')
   $('.content').empty()
-  goTop()
+  // goTop()
+
+  $.toast({
+    text: 'You Have Succesfully Deleted Your Brief! Click Get Briefs To See Updated List',
+    heading: 'Brief Deleted!',
+    icon: 'success',
+    showHideTransition: 'slide',
+    allowToastClose: true,
+    hideAfter: 3000,
+    stack: 5,
+    position: 'top-left',
+    textAlign: 'left',
+    loader: true,
+    loaderBg: '#fed',
+    bgColor: '#green',
+    textColor: 'black'
+  })
 }
 
 const deleteBriefFailure = function () {
-  $('#message').text('Error while updating brief')
-  $('#message').css('background-color', 'red')
+  // $('#message').text('Error while updating brief')
+  // $('#message').css('background-color', 'red')
   $('.content').empty()
-  goTop()
+  // goTop()
+
+  $.toast({
+    text: 'Check your connection',
+    heading: 'Brief Delete Failed',
+    icon: 'warning',
+    showHideTransition: 'slide',
+    allowToastClose: true,
+    hideAfter: 3000,
+    stack: 5,
+    position: 'top-left',
+    textAlign: 'left',
+    loader: true,
+    loaderBg: 'red',
+    bgColor: 'red',
+    textColor: 'black'
+  })
 }
 
 module.exports = {
